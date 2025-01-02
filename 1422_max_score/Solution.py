e550eb0a289de44a2f2158f75badff4f9edcdf1c
Dzenis Madzovic <dzenismadzovic@gmail.com>
2025-01-02 12:24:29 +0100

    Add more efficient solution for 1422

--- 1422_max_score/Solution.py
+++ 1422_max_score/Solution.py
@@ -17,3 +17,27 @@ class Solution:
 s = "011101"
 
 print(Solution().maxScore(s))
+
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+        ones = s.count("1")
+        zeroes = 0
+        score = 0
+
+        for i in range(len(s) - 1):
+            if s[i] == "1":
+                ones -= 1
+            
+            if s[i] == "0":
+                zeroes += 1
+
+            if ones + zeroes > score:
+                score = ones + zeroes
+
+        return score
+
+    
+s = "011101"
+
+print(Solution().maxScore(s))
